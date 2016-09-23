edf42839f9718a29a34e124f125fbcbcdee875db
Troy Blank <troy.blank@topbox.io>
2016-09-23 09:36:15 -0500

    added es5 rules based on es6

---
+++ configs/es5.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        './es6.js'
+    ],
+    rules: {
+        'func-names': 0,
+        'import/no-amd': 0,
+        'no-var': 0,
+        'object-shorthand': 0,
+        'prefer-arrow-callback': 0,
+        'prefer-template': 0,
+        strict: 0
+    }
+};
